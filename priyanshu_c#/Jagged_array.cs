9e0b6649b1a56d75380707892ac051b386072e89
priyanshugajera <priyanshugajera5@gmail.com>
2025-02-19 12:28:34 +0530

    Add files via upload

--- priyanshu_c#/Jagged_array.cs
+++ priyanshu_c#/Jagged_array.cs
@@ -1,42 +1,41 @@
 using System;
-class Jagged_array
-{
+public {
 	public static void Main()
 	{
-		int[][] a=new int[3][];
-		a[0]=new int[2]{1,2};
-		a[1]=new int[4]{4,5,6,8};
-		a[2]=new int[3]{7,8,9};
+		Console.Write("enter row length =");
+		int r=Convert.ToInt32(Console.ReadLine());
 		
-		//Console.WriteLine(a[1].Length);
+		
+		int[][] a=new int[r][];
+		int[] col=new int[r];
+		
 		Console.WriteLine("*********************");
 		for(int i=0;i<a.GetLength(0);i++)
 		{
-			if(i==0)
-			{
-				for(int j=0;j<a[0].Length;j++)
-				{
-					Console.Write("{0} ",a[i][j]);				
-				}
-				Console.Write("\n");
-			}
-			if(i==1)
-			{
-				for(int j=0;j<a[1].Length;j++)
-				{
-					Console.Write("{0} ",a[i][j]);				
-				}
-				Console.Write("\n");
-			}
-			if(i==2)
-			{
-				for(int j=0;j<a[2].Length;j++)
-				{
-					Console.Write("{0} ",a[i][j]);				
-				}
-				Console.Write("\n");
-			}
+				Console.Write("enter column[{0}] length =",i);
+			   col[i]=Convert.ToInt32(Console.ReadLine());
 		}
+		Console.WriteLine("*********************");
+		//Console.WriteLine(a.Length);
+		for(int i=0;i<a.GetLength(0);i++)
+		{
+		
+				for(int j=0;j<a[i].Length;j++)
+				{
+					Console.Write("a[{0}][{1}]:",i,j);
+					a[i][j]=Convert.ToInt32(Console.ReadLine());
+				}
+		} 
+		
+		/*for(int i=0;i<a.GetLength(0);i++)
+		{
+		
+				for(int j=0;j<a[i].Length;j++)
+				{
+					Console.Write("a[{0}{1}]:{2} ",i,j,a[i][j]);				
+				}
+		}*/
+		Console.ReadLine();
 		
 	}
 }
